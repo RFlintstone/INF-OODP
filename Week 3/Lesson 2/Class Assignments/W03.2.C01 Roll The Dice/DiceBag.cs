e0c91b32c58b0192063db3c7ec1534e65dc318b0
RFlintstone <rubenflint@hotmail.com>
2024-02-19 12:39:47 +0100

    Busy with W03.2.C01 - Roll The Dice

---
+++ Week 3/Lesson 2/Class Assignments/W03.2.C01 Roll The Dice/DiceBag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DiceBag
+{
+    public List<Die> Dice = new List<Die>(){}; // contains all Die objects
+
+    /*
+        Add a die to the bag.
+    */
+    public void Add(Die die)
+    {
+        Dice.Add(die);
+    }
+
+    /*
+        Reroll the vallues of all dice in the bag and print info for each die.
+    */
+    public void Reroll() => Dice.ForEach(dice => dice.Roll());
+
+
+    /*
+        Custom method like ToString, but prints directly to the console.
+    */
+    public void Info()
+    {
+        this.Dice.ForEach(die => Console.WriteLine(die));
+    }
+}
---
+++ Week 3/Lesson 2/Class Assignments/W03.2.C01 Roll The Dice/Die.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Die
+{
+    public string Color;
+    public int Sides;
+    public int Value;
+
+    public Die(int sides, string color)
+    {
+        this.Sides = sides;
+        this.Color = color;
+        this.Roll();
+    }
+
+    public void Roll()
+    {
+        Random ran = new Random();
+        this.Value = ran.Next(1, this.Sides + 1);
+    }
+
+    public override string ToString()
+    {
+        return $"sides: {this.Sides}, color: {this.Color}, value: {this.Value}";
+    }
+}
